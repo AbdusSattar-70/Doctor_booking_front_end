d2a87b45fa565737d7707aa99f0ac094b0a1fdb3
Abdus Sattar <sasorganicagro@gmail.com>
2023-07-29 15:23:42 +0600

    Add appointmentInfo components

---
+++ src/components/appointmentInfo/MyAppointments.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyAppointments() {
+  return (
+    <div>
+      <h2>My Appointments List</h2>
+      {/* Display a list of the current user's appointments here */}
+    </div>
+  );
+}
+
+export default MyAppointments;
