1fc643e0badad9701c6e6682dbff47315c3249b8
Abdus Sattar <sasorganicagro@gmail.com>
2023-07-29 15:26:10 +0600

    Add doctorInfo components

---
+++ src/components/doctorInfo/DeleteDoctor.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DeleteDoctor() {
+  return (
+    <div>
+      <h2>Delete Doctor Page</h2>
+      {/* Add your logic and UI to delete a doctor */}
+    </div>
+  );
+}
+
+export default DeleteDoctor;
