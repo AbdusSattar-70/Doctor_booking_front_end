7132cb03da293aa73acafe1eff85a7fb73073f86
Abdus Sattar <sasorganicagro@gmail.com>
2023-07-29 15:30:11 +0600

    Set up app with react and redux

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/homePage/HomePage';
+import SignUpPage from './components/signUpInOut/SignUpForm';
+import SignInPage from './components/signUpInOut/SignInForm';
+import Navbar from './components/navbar/Navbar';
+import AddDoctor from './components/doctorInfo/AddDoctor';
+import DoctorList from './components/doctorInfo/DoctorList';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/sign_up" element={<SignUpPage />} />
+        <Route path="/sign_in" element={<SignInPage />} />
+        <Route path="/add_doctor" element={<AddDoctor />} />
+        <Route path="/doctors" element={<DoctorList />} />
+        {/* Other routes */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
